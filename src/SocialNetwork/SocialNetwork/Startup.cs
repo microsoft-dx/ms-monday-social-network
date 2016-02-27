765f0e638157587575dfa784214535e490fc3ef3
Radu Matei <matei.radu94@gmail.com>
2016-02-27 21:52:46 +0200

    Added Startup class

---
+++ src/SocialNetwork/SocialNetwork/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SocialNetwork.Startup))]
+
+namespace SocialNetwork
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
