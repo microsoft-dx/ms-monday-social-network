87b6ffd266470af18bd6c57adfebc501cd74dcaf
Radu Matei <matei.radu94@gmail.com>
2016-02-27 20:09:00 +0200

    Added PostsController

---
+++ src/SocialNetwork/SocialNetwork/Controllers/PostsController.cs
@@ -0,0 +1,20 @@
+﻿using SocialNetwork.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SocialNetwork.Controllers
+{
+    public class PostsController : ApiController
+    {
+        public List<Post> Posts = new List<Post>() {
+            new Post("Obi-Wan Kenobi","These are not the droids you're looking for"),
+            new Post("Darth Vader","I find your lack of faith disturbing")
+            };
+
+        [HttpGet]
+        public List<Post> GetPosts()
+        {
+            return Posts;
+        }
+    }
+}
