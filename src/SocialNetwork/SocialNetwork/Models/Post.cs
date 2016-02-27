000c390f69c41b0f65bdb2a11d122290897cc24d
Radu Matei <matei.radu94@gmail.com>
2016-02-27 20:08:48 +0200

    Added Post model

---
+++ src/SocialNetwork/SocialNetwork/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace SocialNetwork.Models
+{
+    public class Post
+    {
+        public string UserName { get; set; }
+        public string Text { get; set; }
+
+        public Post(string userName, string text)
+        {
+            UserName = userName;
+            Text = text;
+        }
+    }
+}
